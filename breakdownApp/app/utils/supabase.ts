28497156230429b2e031c3a0ee4fbe3da73d7ebc
Mukelani Mashoba <mukelanilastborn@gmail.com>
2025-09-23 11:35:00 +0200

    Async storage windows error

--- breakdownApp/app/utils/supabase.ts
+++ breakdownApp/app/utils/supabase.ts
@@ -1,19 +1,64 @@
-import { Database } from '@/app/utils/database.types'
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Platform } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient, processLock } from '@supabase/supabase-js'
+import { Database } from '@/app/utils/database.types'
+
+const storageAdapter = {
+  async getItem(key: string) {
+    if (Platform.OS === "web") {
+      return localStorage.getItem(key);
+    }
+    return AsyncStorage.getItem(key);
+  },
+  async setItem(key: string, value: string) {
+    if (Platform.OS === "web") {
+      return localStorage.setItem(key, value);
+    }
+    return AsyncStorage.setItem(key, value);
+  },
+  async removeItem(key: string) {
+    if (Platform.OS === "web") {
+      return localStorage.removeItem(key);
+    }
+    return AsyncStorage.removeItem(key);
+  },
+};
 
 export const supabase = createClient<Database>(
   process.env.EXPO_PUBLIC_SUPABASE_URL!,
   process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!,
   {
-    auth: { 
-      storage: AsyncStorage,
+    auth: {
+      storage: storageAdapter,
       autoRefreshToken: true,
       persistSession: true,
       detectSessionInUrl: false,
-      lock: processLock,
     },
-  })
+  }
+);
 
 
-  
+
+
+
+
+
+
+// import { Database } from '@/app/utils/database.types'
+// import AsyncStorage from '@react-native-async-storage/async-storage'
+// import { createClient, processLock } from '@supabase/supabase-js'
+// //
+// export const supabase = createClient<Database>(
+//   process.env.EXPO_PUBLIC_SUPABASE_URL!,
+//   process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!,
+//   {
+//     auth: {
+//       storage: AsyncStorage,
+//       autoRefreshToken: true,
+//       persistSession: true,
+//       detectSessionInUrl: false,
+//       lock: processLock,
+//     },
+//   })
+
+
